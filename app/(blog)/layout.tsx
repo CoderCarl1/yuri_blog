207fa92c40217ede5f7cb9fe6a6ac9f150104b65
Carl <carl.davidson@hotmail.com>
2024-05-31 13:33:19 +1000

    UPDATE minor styling

--- app/(blog)/layout.tsx
+++ app/(blog)/layout.tsx
@@ -20,9 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {draftMode().isEnabled && (
-          <div>
+          <div className='w-fit py-2.5 px-1'>
             <a
-              className="p-4 bg-blue-400 text-white block border border-transparent hover:bg-indigo-200 hover:border-indigo-500 hover:text-black focus:bg-indigo-200 focus:border-indigo-500 focus:text-black active:bg-indigo-200 active:border-indigo-600 active:text-black"
+              className="rounded-md p-4 bg-blue-400 text-white block border border-transparent hover:bg-indigo-200 hover:border-indigo-500 hover:text-black focus:bg-indigo-200 focus:border-indigo-500 focus:text-black active:bg-indigo-200 active:border-indigo-600 active:text-black"
               href="/api/disable-draft"
             >
               Disable preview mode
