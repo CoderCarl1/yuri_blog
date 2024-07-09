90fedbe5d6fc1b3c0b36a03206717f34e28ef95a
Carl <carl.davidson@hotmail.com>
2024-07-09 12:02:54 +1000

    separate sanity fetch from native fetch

---
+++ sanity/lib/fetch.client.ts
@@ -0,0 +1,34 @@
+import { Post_SanityDocument } from '@/components/Post';
+
+export async function searchAPI(
+    str: string,
+    signal: AbortSignal | null | undefined = null,
+): Promise<Post_SanityDocument[]>{
+
+    const url = `/api/search?query=${str}`;
+    let results;
+
+    try {
+        results = await apiFetch<Post_SanityDocument[]>(url, signal);
+
+    } catch (err) {
+        console.log(err);
+    } finally {
+        return results || [];
+    }
+
+}
+
+export async function apiFetch<QueryResponse>(
+    url: string,
+    signal: AbortSignal | null | undefined = null,
+  ){
+    console.log("fetch request made")
+    const response = await fetch(url, { signal });
+  
+    if (!response.ok) {
+      throw new Error(`HTTP error: Status ${response.status}`);
+    }
+  
+    return response.json() as Promise<QueryResponse>;
+  };
