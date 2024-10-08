e81497db0be01a1bffe9d1fcc29b0479c16d2f7d
Carl <carl.davidson@hotmail.com>
2024-10-08 12:14:54 +1000

    fix possible empty obj

--- sanity/components/JSONPreview.tsx
+++ sanity/components/JSONPreview.tsx
@@ -2,7 +2,7 @@ import { type componentParamsType } from '@/types';
 
 export default function JsonPreview({ document }: componentParamsType) {
   const { displayed } = document;
-  const title = displayed?.title || 'title not found';
+  const title: string = typeof displayed.title === 'string' ? displayed.title : 'title not found';
   return (
     <div>
       <h1>JSON Data for "{title}"</h1>
